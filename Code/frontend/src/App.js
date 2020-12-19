05fb05f6de49afdbafa147fb5c70d9e48353e988
ilmiat <55845786+ilmiat@users.noreply.github.com>
2020-12-20 01:21:42 +0600

    transformed my products

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
+import data from './data'
 import './App.css';
 
 function App() {
@@ -18,61 +18,21 @@ function App() {
       <main className="main">
         <div className="content">
           <ul className="products">
-            <li>
-              <div className="product">
-                <img className="image" src="ssd.jpg" alt="SSD" />
-                <div className="product-name">
-                  <a href="product.html"> SSD 256gb </a>
-                </div>
-                <div className="brand"> WD</div>
-                <div className="price">$30</div>
-                <div className="rating">3.5 stars</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="image" src="Sandisk.jpg" alt="Sandisk" />
-                  <div className="product-name">
-                    <a href="product.html"> SSD 120GB </a>
-                  </div>
-                  <div className="brand"> Sandisk</div>
-                  <div className="price">$17</div>
-                  <div className="rating">2.2 stars</div>
-            </div>
-           </li>
+            {
+              data.products.map (product =>
+              
               <li>
-                <div className="product">
-                  <img className="image" src="Transcend.jpg" alt="Transcend" />
-                    <div className="product-name">
-                      <a href="product.html"> SSD 128gb </a>
-                    </div>
-                    <div className="brand"> Transcend</div>
-                    <div className="price">$20</div>
-                    <div className="rating">3 stars</div>
+              <div className="product">
+                <img className="image" src={product.image} alt="SSD" />
+                <div className="product-name">
+                  <a href="product.html">{product.name}</a>
                 </div>
-              </li>
-                <li>
-                  <div className="product">
-                    <img className="image" src="HP.jpg" alt="HP" />
-                      <div className="product-name">
-                        <a href="product.html"> SSD 128gb </a>
-                      </div>
-                      <div className="brand"> HP</div>
-                      <div className="price">$25</div>
-                      <div className="rating">3.5 stars</div>
-                 </div>
-                </li>
-                  <li>
-                    <div className="product">
-                      <img className="image" src="WD BLUE.png" alt="WD Blue" />
-                        <div className="product-name">
-                          <a href="product.html"> SSD 240gb </a>
-                        </div>
-                        <div className="brand"> WD BLUE</div>
-                        <div className="price">$28</div>
-                        <div className="rating">2.5 stars</div>
-                  </div>
-                </li>
+                <div className="brand"> {product.brand}</div>
+                <div className="price">${product.price}</div>
+                <div className="rating">{product.rating}Stars {product.numeviews}</div>
+              </div>
+              </li>)
+            } 
           </ul>
       </div>
     </main>
---
+++ Code/frontend/src/data.js
@@ -0,0 +1,29 @@
+export default {
+    products:[
+        
+    {
+        name: 'SSD 120gb',
+        image: '/images/Sandisk.jpg',
+        price: 17,
+        brand: 'Sandisk',
+        rating: 2.5,
+    },
+    {
+        name: 'SSD 120gb',
+        image: '/images/.ssd.jpg',
+        price: 17,
+        brand: 'WD',
+        rating: 4.5,
+    },
+    {
+        name: 'SSD 120gb',
+        image: '/images/HP.jpg',
+        price: 17,
+        brand: 'HP',
+        rating: 2.5,
+    },
+
+
+
+]
+}
