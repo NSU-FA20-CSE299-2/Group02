19e03bdb209a98bd5f3846edb3899c631a17cc6d
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2020-12-21 14:59:00 +0600

    Bug #30 Stopped treating warnings as error
    Treating warnings as errors because process.env.CI = true.

--- Code/frontend/src/data.js
+++ Code/frontend/src/data.js
@@ -1,37 +1,33 @@
 export default {
-    products:[
-        
-    {
-        name: 'SSD 120gb',
-        image: '/images/Sandisk.jpg',
-        price: 15,
-        brand: 'Sandisk',
-        rating: 2.5,
-    },
-    {
-        name: 'SSD 120gb',
-        image: '/images/.ssd.jpg',
-        price: 17,
-        brand: 'WD',
-        rating: 4.5,
-    },
-    {
-        name: 'SSD 120gb',
-        image: '/images/HP.jpg',
-        price: 36,
-        brand: 'HP',
-        rating: 2.5,
-    },
-    {
-        name: 'SSD 500gb',
-        image: '/images/Transcend.jpg',
-        price: 20,
-        brand: 'Transcend',
-        rating: 3.5,
-    },
+    products: [
 
-
-
-
-]
+        {
+            name: 'SSD 120gb',
+            image: '/images/Sandisk.jpg',
+            price: 15,
+            brand: 'Sandisk',
+            rating: 2.5,
+        },
+        {
+            name: 'SSD 120gb',
+            image: '/images/.ssd.jpg',
+            price: 17,
+            brand: 'WD',
+            rating: 4.5,
+        },
+        {
+            name: 'SSD 120gb',
+            image: '/images/HP.jpg',
+            price: 36,
+            brand: 'HP',
+            rating: 2.5,
+        },
+        {
+            name: 'SSD 500gb',
+            image: '/images/Transcend.jpg',
+            price: 20,
+            brand: 'Transcend',
+            rating: 3.5,
+        },
+    ]
 }
