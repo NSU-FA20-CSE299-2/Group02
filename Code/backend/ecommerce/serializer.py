dfc4f68970a255f00268b55b41bd31caf75fb693
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2020-12-28 12:54:48 +0600

    Enhancement #38 Created Serialisers for E commerce

--- Code/backend/ecommerce/serializer.py
+++ Code/backend/ecommerce/serializer.py
@@ -1,2 +1,26 @@
 from rest_framework import serializers
 from ecommerce.models import *
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields - '__all__'
