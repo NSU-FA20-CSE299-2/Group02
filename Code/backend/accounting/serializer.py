feacc41bd2f576d1fae79afe7e29f8fbf929ff9f
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2020-12-28 12:55:08 +0600

    Enhancement #42 Created Serializers for Accouting

--- Code/backend/accounting/serializer.py
+++ Code/backend/accounting/serializer.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from accounting.models import *
+
+class TransactionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transaction
+        fields = '__all__'
