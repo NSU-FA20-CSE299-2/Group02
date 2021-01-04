c9113b8f0ca2efe334a0df6e8ed1998a64ba9db1
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2021-01-04 16:38:46 +0600

    Enhancement #58 Create Accounting API

--- Code/backend/accounting/api.py
+++ Code/backend/accounting/api.py
@@ -1,4 +1,19 @@
 from accounting.models import *
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, authentication
 from .serializer import *
-from rest_framework.authentication import TokenAuthentication
+
+class TransactionViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    authentication_classes = [
+        authentication.TokenAuthentication
+    ]
+
+    serializer_class = TransactionSerializer
+    
+    def get_queryset(self):
+        queryset = Transaction.objects.all()
+        return queryset
