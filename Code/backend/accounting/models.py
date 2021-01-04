94cc5221818e08974ba7136f95f333c8fd62e8a7
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2021-01-04 16:48:21 +0600

    Bug #59 Imported uuid

--- Code/backend/accounting/models.py
+++ Code/backend/accounting/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from account.models import User
+import uuid
 
 class Transaction(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
