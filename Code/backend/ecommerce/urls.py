1c6eda242dd39f1e64789396bf7e871182ce58c3
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2021-01-04 16:38:25 +0600

    Enhancement #54 Created E Commmerce URLS

--- Code/backend/ecommerce/urls.py
+++ Code/backend/ecommerce/urls.py
@@ -1,6 +1,15 @@
 from .api import *
 from django.urls import path
+from rest_framework import routers
 
-urlpatterns = [
-    
-]
+urlpatterns = []
+
+router = routers.DefaultRouter()
+
+router.register('category', CategoryViewSet, 'category')
+router.register('item', ItemViewSet, 'item')
+router.register('orderitem', OrderItemViewSet, 'orderitem')
+router.register('order', OrderViewSet, 'order')
+router.register('payment', PaymentViewSet, 'payment')
+
+urlpatterns += router.urls
