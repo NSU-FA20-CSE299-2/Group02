2df65237faa4efc061ea6e29455f451e147342c3
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2021-01-04 12:42:57 +0600

    Enhancement #49 Created API Endpoints
    - Need to modify the querysets. Endpoints not finalized

--- Code/backend/ecommerce/api.py
+++ Code/backend/ecommerce/api.py
@@ -2,3 +2,83 @@ from ecommerce.models import *
 from rest_framework import viewsets, permissions
 from .serializer import *
 from rest_framework.authentication import TokenAuthentication
+
+class CategoryViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    authentication_classes = [
+        authentication.TokenAuthentication
+    ]
+
+    serializer_class = CategorySerializer
+    
+    def get_queryset(self):
+        queryset = Category.objects.all()
+        return queryset
+
+class ItemViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    authentication_classes = [
+        authentication.TokenAuthentication
+    ]
+
+    serializer_class = ItemSerializer
+    
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        return queryset
+
+class OderItemViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    authentication_classes = [
+        authentication.TokenAuthentication
+    ]
+
+    serializer_class = OrderItemSerializer
+    
+    def get_queryset(self):
+        queryset = OrderItem.objects.all()
+        return queryset
+
+class OrderViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    authentication_classes = [
+        authentication.TokenAuthentication
+    ]
+
+    serializer_class = ItemSerializer
+    
+    def get_queryset(self):
+        queryset = Order.objects.all()
+        return queryset
+
+class PaymentViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    authentication_classes = [
+        authentication.TokenAuthentication
+    ]
+
+    serializer_class = PaymentSerializer
+    
+    def get_queryset(self):
+        queryset = Payment.objects.all()
+        return queryset
