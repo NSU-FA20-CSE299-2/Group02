7ad95ad1aed7c055e46766de714ea7df610b560e
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2021-01-18 02:04:57 +0600

    Enchancement #61 Updated APIviews and URL

--- Code/backend/ecommerce/api.py
+++ Code/backend/ecommerce/api.py
@@ -1,83 +1,148 @@
-from ecommerce.models import *
-from rest_framework import viewsets, permissions, authentication
-from .serializer import *
+from django.db.models import Q
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from rest_framework.generics import (
+    ListAPIView, RetrieveAPIView, CreateAPIView,
+    UpdateAPIView, DestroyAPIView
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from .serializers import (
+    ItemSerializer, OrderSerializer, ItemDetailSerializer, AddressSerializer,
+    PaymentSerializer
+)
+from ecommerce.models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile, Variation, ItemVariation
 
-class CategoryViewSet(viewsets.ModelViewSet):
 
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
+class UserIDView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({'userID': request.user.id}, status=HTTP_200_OK)
 
-    authentication_classes = [
-        authentication.TokenAuthentication
-    ]
 
-    serializer_class = CategorySerializer
-    
-    def get_queryset(self):
-        queryset = Category.objects.all()
-        return queryset
-
-class ItemViewSet(viewsets.ModelViewSet):
-
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
-
-    authentication_classes = [
-        authentication.TokenAuthentication
-    ]
-
+class ItemListView(ListAPIView):
+    permission_classes = (AllowAny,)
     serializer_class = ItemSerializer
-    
-    def get_queryset(self):
-        queryset = Item.objects.all()
-        return queryset
+    queryset = Item.objects.all()
 
-class OrderItemViewSet(viewsets.ModelViewSet):
 
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
+class ItemDetailView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ItemDetailSerializer
+    queryset = Item.objects.all()
 
-    authentication_classes = [
-        authentication.TokenAuthentication
-    ]
 
-    serializer_class = OrderItemSerializer
-    
-    def get_queryset(self):
-        queryset = OrderItem.objects.all()
-        return queryset
+class OrderQuantityUpdateView(APIView):
+    def post(self, request, *args, **kwargs):
+        slug = request.data.get('slug', None)
+        if slug is None:
+            return Response({"message": "Invalid data"}, status=HTTP_400_BAD_REQUEST)
+        item = get_object_or_404(Item, slug=slug)
+        order_qs = Order.objects.filter(
+            user=request.user,
+            ordered=False
+        )
+        if order_qs.exists():
+            order = order_qs[0]
+            # check if the order item is in the order
+            if order.items.filter(item__slug=item.slug).exists():
+                order_item = OrderItem.objects.filter(
+                    item=item,
+                    user=request.user,
+                    ordered=False
+                )[0]
+                if order_item.quantity > 1:
+                    order_item.quantity -= 1
+                    order_item.save()
+                else:
+                    order.items.remove(order_item)
+                return Response(status=HTTP_200_OK)
+            else:
+                return Response({"message": "This item was not in your cart"}, status=HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"message": "You do not have an active order"}, status=HTTP_400_BAD_REQUEST)
 
-class OrderViewSet(viewsets.ModelViewSet):
 
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
+class OrderItemDeleteView(DestroyAPIView):
+    permission_classes = (IsAuthenticated, )
+    queryset = OrderItem.objects.all()
 
-    authentication_classes = [
-        authentication.TokenAuthentication
-    ]
 
-    serializer_class = ItemSerializer
-    
-    def get_queryset(self):
-        queryset = Order.objects.all()
-        return queryset
+class AddToCartView(APIView):
+    def post(self, request, *args, **kwargs):
+        slug = request.data.get('slug', None)
+        variations = request.data.get('variations', [])
+        if slug is None:
+            return Response({"message": "Invalid request"}, status=HTTP_400_BAD_REQUEST)
 
-class PaymentViewSet(viewsets.ModelViewSet):
+        item = get_object_or_404(Item, slug=slug)
 
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
+        minimum_variation_count = Variation.objects.filter(item=item).count()
+        if len(variations) < minimum_variation_count:
+            return Response({"message": "Please specify the required variation types"}, status=HTTP_400_BAD_REQUEST)
 
-    authentication_classes = [
-        authentication.TokenAuthentication
-    ]
+        order_item_qs = OrderItem.objects.filter(
+            item=item,
+            user=request.user,
+            ordered=False
+        )
+        for v in variations:
+            order_item_qs = order_item_qs.filter(
+                Q(item_variations__exact=v)
+            )
 
-    serializer_class = PaymentSerializer
-    
-    def get_queryset(self):
-        queryset = Payment.objects.all()
-        return queryset
+        if order_item_qs.exists():
+            order_item = order_item_qs.first()
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order_item = OrderItem.objects.create(
+                item=item,
+                user=request.user,
+                ordered=False
+            )
+            order_item.item_variations.add(*variations)
+            order_item.save()
+
+        order_qs = Order.objects.filter(user=request.user, ordered=False)
+        if order_qs.exists():
+            order = order_qs[0]
+            if not order.items.filter(item__id=order_item.id).exists():
+                order.items.add(order_item)
+                return Response(status=HTTP_200_OK)
+
+        else:
+            ordered_date = timezone.now()
+            order = Order.objects.create(
+                user=request.user, ordered_date=ordered_date)
+            order.items.add(order_item)
+            return Response(status=HTTP_200_OK)
+
+
+class OrderDetailView(RetrieveAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            return order
+        except ObjectDoesNotExist:
+            raise Http404("You do not have an active order")
+
+
+class AddCouponView(APIView):
+    def post(self, request, *args, **kwargs):
+        code = request.data.get('code', None)
+        if code is None:
+            return Response({"message": "Invalid data received"}, status=HTTP_400_BAD_REQUEST)
+        order = Order.objects.get(
+            user=self.request.user, ordered=False)
+        coupon = get_object_or_404(Coupon, code=code)
+        order.coupon = coupon
+        order.save()
+        return Response(status=HTTP_200_OK)
--- Code/backend/ecommerce/urls.py
+++ Code/backend/ecommerce/urls.py
@@ -1,15 +1,25 @@
-from .api import *
 from django.urls import path
-from rest_framework import routers
+from .api import (
+    UserIDView,
+    ItemListView,
+    ItemDetailView,
+    AddToCartView,
+    OrderDetailView,
+    OrderQuantityUpdateView,
+    AddCouponView,
+    OrderItemDeleteView,
+)
 
-urlpatterns = []
-
-router = routers.DefaultRouter()
-
-router.register('category', CategoryViewSet, 'category')
-router.register('item', ItemViewSet, 'item')
-router.register('orderitem', OrderItemViewSet, 'orderitem')
-router.register('order', OrderViewSet, 'order')
-router.register('payment', PaymentViewSet, 'payment')
-
-urlpatterns += router.urls
+urlpatterns = [
+    path('user-id/', UserIDView.as_view(), name='user-id'),
+    path('products/', ItemListView.as_view(), name='product-list'),
+    path('products/<pk>/', ItemDetailView.as_view(), name='product-detail'),
+    path('add-to-cart/', AddToCartView.as_view(), name='add-to-cart'),
+    path('order-summary/', OrderDetailView.as_view(), name='order-summary'),
+    path('checkout/', PaymentView.as_view(), name='checkout'),
+    path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
+    path('order-items/<pk>/delete/',
+         OrderItemDeleteView.as_view(), name='order-item-delete'),
+    path('order-item/update-quantity/',
+         OrderQuantityUpdateView.as_view(), name='order-item-update-quantity'),
+]
